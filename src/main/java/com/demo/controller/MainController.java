710d74d3b35afcf082abf4a4ed4b82eac6eaeb79
sbm0709 <130350291+sbm0709@users.noreply.github.com>
2024-01-04 18:31:02 +0900

    Initial commit

---
+++ src/main/java/com/demo/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.demo.controller;
+
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+
+@Log4j2
+@Controller
+public class MainController {
+
+
+
+}
